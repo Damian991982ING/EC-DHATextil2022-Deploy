471c75c37e031c5ec160f063aa6d657e36da9f88
Damian991982 <damian991982wwe@gmail.com>
2022-04-15 21:39:23 -0300

    Menu

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Nabvar from './components/Navbar/Navbar';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Crear un Ecommerce con React y Firebase
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nabvar/>
+     
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+import "./Navbar.css";
+
+
+const Nabvar=()=>{
+    return(
+        <header className="header navbar navbar-expand-lg navbar-light bg-light">
+            <div className="logo">
+                <h1><a href="/">DHATextil</a></h1>
+            </div>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/">Products</a></li>
+                <li><a href="/">About</a></li>
+                <li><a href="/">Contact</a></li>
+                <li>
+                    
+                </li>
+            </ul>
+            <div className="menu">
+                <img src="" alt="/"/>
+                
+
+            </div>
+        </header>
+
+       
+
+    );
+};
+
+export default Nabvar;
