c6dc80cb5fc1ae25fb834a176585de0f89e58cfb
Damian991982 <damian991982wwe@gmail.com>
2022-04-22 01:10:27 -0300

    Map y Promises 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
-
 import './App.css';
-import Greeting from './components/Greeting/Greeting';
-import ItemCount from './components/ItemCount/ItemCount';
-import ItemList from './components/ItemList/ItemList';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import Nabvar from './components/Navbar/Navbar';
 
@@ -12,10 +8,8 @@ function App() {
   return (
     <div className="App">
       <Nabvar/>
-      <ItemListContainer greeting="Hola soy un componente funcional desde una prop"/>
-      <ItemCount initial={1} min={0} max={10} />
-      <ItemList/>
-      <Greeting greeting="Welcome to DHATextil's e-commerce"/>
+      <ItemListContainer titleSection="All products"/>
+     
      
     </div>
   );
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,13 +1,63 @@
-import React from "react";
-import {} from "react-router-dom";
+import React,{useState,useEffect} from "react";
+import Loading from "../Loading/Loading";
+import ItemList from "../ItemList/ItemList";
 import "./ItemListContainer.css";
+import BH1 from "../../assets/BH1.jpg";
+import BH2 from "../../assets/BH2.jpg";
+import BH3 from "../../assets/BH3.jpg";
+import BH4 from "../../assets/BH4.jpg";
+import BH5 from "../../assets/BH5.jpg";
+import BH6 from "../../assets/BH6.jpg";
 
-const ItemListContainer=(props)=>{
+const objProducts = [
+    {id:1,title:"BH1",description:"Polyamide thread spool",category:"Polyamide",price:100,stock:10,pictureUrl:BH1},
+    {id:2,title:"BH2",description:"Polyester thread spool",category:"Polyester",price:200,stock:10,pictureUrl:BH2},
+    {id:3,title:"BH3",description:"Acrylic thread spool",category:"Acrylic",price:300,stock:10,pictureUrl:BH3},
+    {id:4,title:"BH4",description:"Plasma polymerization polyamide thread spool",category:"Polyamide",price:200,stock:10,pictureUrl:BH4},
+    {id:5,title:"BH5",description:"Plasma polimerization polyester thread spool",category:"Polyester",price:400,stock:10,pictureUrl:BH5},
+    {id:6,title:"BH6",description:"Plasma polimerization acrylic thread spool",category:"Acrylic",price:600,stock:10,pictureUrl:BH6},
+
+];
+
+
+
+
+const ItemListContainer = ({titleSection}) => {
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading]= useState(true);
+
+    useEffect(()=>{
+
+        const task = new Promise((resolve, reject)=>{
+            setTimeout(()=>{
+
+                resolve(objProducts);
+            },2000)});
+
+            task.then(
+                res=>{
+                    setProducts(res);
+                    setLoading(false)
+                },
+                err=>{console.log(err)},
+            );
+            task.catch(err=>{
+                console.log(err);
+            });
+            task.finally(()=>{
+                console.log("Se resolvio la promesa");
+            });
+    }, []);
+
+
     return(
-        <div className="title-prop">
-            <h2>{props.greeting}</h2>
-
+        <div id="ItemListContainer" className="mt-3 container">
+            <h1>{titleSection}</h1>
+            {loading && <Loading/>}
+            <ItemList items={products}/>
+            
         </div>
+       
 
         
 
